aa24b2d43f505b2a6ee2d0682b6e429f3da8975b
Aison0577 <126125120+Aison0577@users.noreply.github.com>
2024-10-19 11:34:51 -0700

    New

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,8 @@ import Heading from './Heading'
 import ProjectCard from './ProjectCard'
 import p1 from '../assets/im.jpg'
 import p2 from '../assets/HOME0.png'
+import p3 from '../assets/wu.png'
+import p4 from '../assets/rn.png'
 // import { Stack } from '@mui/material'
 
 export default function Projects() {
@@ -12,11 +14,10 @@ export default function Projects() {
       <div className='w-full grid md:grid-cols-3 gap-3'>
         <ProjectCard link={'https://github.com/Aison0577/Management_Project'} name={'Inventory Management Application'} image={p1 }/>
         <ProjectCard link={'https://github.com/Aison0577/Ecomm'} name={'E-Commerce Website'} image={p2 }/>
+        <ProjectCard link={'https://github.com/Aison0577/Ecomm'} name={'Write Up'} image={p3}/>
+        <ProjectCard link={'https://github.com/Aison0577/Ecomm'} name={'Rede Naturals'} image={p4}/>
       </div>
-      {/* <Stack direction={'row'}>
-        <ProjectCard link={'https://github.com/Aison0577/Management_Project'} name={'Inventory Management Application'} image={p1 }/>
-        <ProjectCard link={'https://github.com/Aison0577/Ecomm'} name={'E-Commerce Website'} image={p2 }/>
-      </Stack> */}
+      
       <hr className='my-2'/>
     </div>
   )
