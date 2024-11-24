2348af5d9785b99b016e5e1095c3f419c6117bd6
Aison0577 <126125120+Aison0577@users.noreply.github.com>
2024-11-23 21:03:39 -0800

    cards

--- src/components/TechStacks.jsx
+++ src/components/TechStacks.jsx
@@ -3,10 +3,15 @@ import TechStackCard from './TechStackCard'
 import node from '../assets/node.png'
 import react from '../assets/react.png'
 import php from '../assets/php.png'
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import tw from '../assets/tw.png'
+import mysql from '../assets/mysql.png'
+import mongodb from '../assets/mongodb.png'
 
 export default function TechStacks() {
   return (
-    <div className='overflow-hidden w-full'>
+    <div className='overflow-hidden w-full py-4'>
         <div className='flex w-[calc(200%)] gap-4 animate-slide'>
             <TechStackCard
                 name={'Hello'}
@@ -20,7 +25,26 @@ export default function TechStacks() {
                 name={'Hello'}
                 logo={php}
             />
-
+            <TechStackCard
+                name={'Hello'}
+                logo={html}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={css}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={tw}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={mysql}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={mongodb}
+            />
 
             <TechStackCard
                 name={'Hello'}
@@ -34,6 +58,28 @@ export default function TechStacks() {
                 name={'Hello'}
                 logo={php}
             />
+            <TechStackCard
+                name={'Hello'}
+                logo={html}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={css}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={tw}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={mysql}
+            />
+            <TechStackCard
+                name={'Hello'}
+                logo={mongodb}
+            />
+
+           
            
         </div>
     </div>
