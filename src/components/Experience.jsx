6e40c481b319c17b188bebbbc46c4ecec5aac151
Aison0577 <126125120+Aison0577@users.noreply.github.com>
2024-12-17 10:50:12 -0800

    new

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -12,13 +12,13 @@ export default function Experience() {
           date={'2023-2024'} 
           company={'University of Cape Coast, MIS'} 
           title={'Junior Programmer'}
-          description={'This experinece helped me achieve effieciency in web deveopment using Tailwind CSS, with the aim of creating Responsive UI'}
+          description={'At the University of Cape Coast MIS, I gained valuable experience in web development, particularly with Tailwind CSS. This opportunity allowed me to create responsive and visually appealing web interfaces, enhancing my skills in modern web design and development.'}
         />
         <SectionElements 
           date={'2023-2024'} 
           company={'Joandy Grafix, Koforidua'} 
           title={'Graphic Designer'}
-          description={'This experinece helped me achieve effieciency in web deveopment using Tailwind CSS, with the aim of creating Responsive UI'}
+          description={'During my internship at Joandy Grafix, a premier graphic design company in Koforidua, I gained hands-on experience in creating compelling visual designs, collaborating with clients, and mastering tools essential for professional graphic design. This experience honed my creativity and strengthened my ability to deliver impactful designs.'}
         />
       </div>
       <hr className='my-2'/>
--- src/components/TechStackCard.jsx
+++ src/components/TechStackCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function TechStackCard({logo,name}) {
   return (
-    <div className='w-full  md:w-[30%] flex items-center justify-center p-2 rounded-md bg-slate-100'>
+    <div className='w-full shadow-lg md:max-w-[30%] flex items-center justify-center p-2 rounded-md bg-slate-100'>
         <img src={logo} alt=""  className='size-10'/>
       {/* {name} */}
     </div>
