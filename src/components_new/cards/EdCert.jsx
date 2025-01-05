bb5823402869a8e4f4d6a7c3ad37a8f57da25f23
Aison0577 <126125120+Aison0577@users.noreply.github.com>
2025-01-05 00:23:06 +0000

    update

--- src/components_new/cards/EdCert.jsx
+++ src/components_new/cards/EdCert.jsx
@@ -4,8 +4,8 @@ export default function EdCert({year,course,school}) {
   return (
     <div className='py-4 '>
       <h1 className='text-sm text-blue-700'>{year}</h1>
-      <h1 className='text-lg font-medium'>{course}</h1>
-      <h1 className='text-lg font-medium'>{school}</h1>
+      <h1 className='text-base md:text-lg font-medium'>{course}</h1>
+      <h1 className='text-base md:text-lg font-medium'>{school}</h1>
     </div>
   )
 }
--- src/components_new/sections/ExploreSections/Education_Certs.jsx
+++ src/components_new/sections/ExploreSections/Education_Certs.jsx
@@ -5,7 +5,7 @@ export default function Education_Certs() {
   return (
     <div className='p-4 text-black dark:text-white border-b-[1px] border-gray-800'>
         <h1 className='mb-3 font-semibold text-2xl'>Education</h1>
-            <div className='grid md:grid-cols-2'>
+            <div className='grid md:grid-cols-2 gap-2'>
                 <EdCert
                     year={'2017-2020'}
                     course={'Science ICT'}
@@ -20,7 +20,7 @@ export default function Education_Certs() {
         <hr className='border-b-[1px] border-gray-800 mb-5'/>
 
         <h1 className='mb-3 font-semibold text-2xl'>Certifications</h1>
-            <div className='grid md:grid-cols-2'>
+            <div className='grid md:grid-cols-2 gap-2'>
                 <EdCert
                     year={'2024'}
                     course={"Bachelor's Degree in Computer Science"}
